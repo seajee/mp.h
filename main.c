c2d49c9a29c49e2c23c063a836283f9e404f8f18
seajee <teapods@proton.me>
2025-03-03 00:52:46 +0100

    Set VM variables from REPL

--- main.c
+++ main.c
@@ -8,6 +8,13 @@
 
 #define INPUT_BUFFER_CAPACITY 512
 
+void vm_set_vars(Vm *vm, double vars[26])
+{
+    for (size_t i = 0; i < 26; ++i) {
+        vm_var(vm, i + 'a', vars[i]);
+    }
+}
+
 int main(void)
 {
     bool quit = false;
@@ -17,6 +24,9 @@ int main(void)
     Arena arena = {0};
     Token_List token_list = {0};
     Parse_Tree parse_tree = {0};
+    double vars[26];
+
+    memset(vars, 0, sizeof(vars));
 
     printf("Type `help` for more information\n");
 
@@ -30,25 +40,52 @@ int main(void)
 
         if (strcmp(input, "help\n") == 0) {
             printf("Usage:\n");
-            printf("  Type an expression or a command to use the application\n");
-            printf("  Supported operations: + - * /\n");
-            printf("  Example:\n");
-            printf("    2 * (4.3 / 3.1) - 8\n");
+            printf("  Type an expression or a command to use the application.\n");
+            printf("  Supported operations: (+) (-) (*) (/)\n");
+            printf("  Example: 2 * (4.3 / 3.1) - 8\n");
             printf("\n");
             printf("Commands:\n");
             printf("  debug:    Toggle debug information\n");
             printf("  exit:     Quit the application\n");
             printf("  help:     Show this text\n");
+            printf("  set:      Set a variable to a specific value (a - z)\n");
             continue;
-        } else if (strcmp(input, "exit\n") == 0) {
-            quit = true;
-            goto cleanup;
+
         } else if (strcmp(input, "debug\n") == 0) {
             debug = !debug;
             if (debug)
                 printf("on\n");
             else
                 printf("off\n");
+            continue;
+
+        } else if (strcmp(input, "exit\n") == 0) {
+            quit = true;
+            goto cleanup;
+
+        } else if (strcmp(input, "set\n") == 0) {
+            printf("var: ");
+            if (fgets(input, INPUT_BUFFER_CAPACITY, stdin) == NULL)
+                goto cleanup;
+            char var = input[0];
+
+            if (var < 'a' || var > 'z') {
+                printf("ERROR: Invalid variable name (a - z)\n");
+                continue;
+            }
+
+            printf("value: ");
+            if (fgets(input, INPUT_BUFFER_CAPACITY, stdin) == NULL)
+                goto cleanup;
+            char *end = NULL;
+            double num = strtod(input, &end);
+            if (input == end) {
+                printf("ERROR: Expected a number\n");
+                continue;
+            }
+
+            vars[var - 'a'] = num;
+            printf("%c = %.10g\n", var, num);
 
             continue;
         }
@@ -101,19 +138,18 @@ int main(void)
         }
 
         Vm vm = vm_init(program);
-
+        vm_set_vars(&vm, vars);
         if (!vm_run(&vm)) {
             printf("ERROR: Error while executing VM program\n");
             vm_free(&vm);
             da_free(&program);
             goto reset;
         }
-
         result = vm_result(&vm);
+        printf("%.10g\n", result);
+
         vm_free(&vm);
         da_free(&program);
-
-        printf("%.10g\n", result);
 reset:
         arena_reset(&arena);
         da_reset(&token_list);
